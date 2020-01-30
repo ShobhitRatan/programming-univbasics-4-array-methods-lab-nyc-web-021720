21b5778afc09e2c933d8ed6e46a129cd4ebe32b8
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-30 00:59:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,13 @@
 def using_include(array, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element1 = "wow"
+  element2 = "Tardis"
+  array.include?(element1)
+  array.include?(element2)
+  using_include(array, element1)
+  using_include(array, element2) 
 end
+
 
 def using_sort(array)
 
