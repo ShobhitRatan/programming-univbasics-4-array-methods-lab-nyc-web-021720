31a2df50123b6823c0adc42015e776da5525c978
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-30 01:04:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  element2 = "Tardis"
-  array.include?("wow")
-  array.include?("Tardis")
+  array.include?(element)
 end
 
 
